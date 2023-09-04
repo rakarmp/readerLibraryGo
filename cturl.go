0428ef1b05a9e0ed8a3a1871282017475e70a144
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-09-04 18:13:45 +0700

    Initial Commit

---
+++ cturl.go
@@ -0,0 +1,5 @@
+package main
+
+type WebsiteUrl struct {
+	URL string
+}
---
+++ reader.go
@@ -0,0 +1,36 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func (r *WebsiteUrl) Read(p []byte) (n int, err error) {
+	res, err := http.Get(r.URL)
+
+	if err != nil {
+		return 0, err
+	}
+
+	defer res.Body.Close()
+	return res.Body.Read(p)
+}
+
+func WebsiteReader(URL string) *WebsiteUrl {
+	return &WebsiteUrl{URL: URL}
+}
+
+func main() {
+	reader := WebsiteReader("https://github.com/rakarmp/astro-shadcn-ui")
+
+	data := make([]byte, 1024)
+
+	n, err := reader.Read(data)
+
+	if err != nil {
+		fmt.Println("Error : ", err)
+		return
+	}
+
+	fmt.Printf("Read %d bytes: %s\n", n, data[:n])
+}
