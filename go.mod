93530594c2dbc4e9f90bff5e1f48a955328b1644
rakarmp <raka_088@smkassalaambandung.sch.id>
2023-09-04 19:31:17 +0700

    Implement Extract data with WriteFile

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module readerGo
 
 go 1.21.0
+
+require golang.org/x/net v0.14.0 // indirect
--- reader.go
+++ reader.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
+	"os"
+
+	"golang.org/x/net/html"
 )
 
 func (r *WebsiteUrl) Read(p []byte) (n int, err error) {
@@ -13,17 +17,45 @@ func (r *WebsiteUrl) Read(p []byte) (n int, err error) {
 	}
 
 	defer res.Body.Close()
-	return res.Body.Read(p)
+
+	doc, err := html.Parse(res.Body)
+
+	if err != nil {
+		return 0, err
+	}
+
+	data := extractData(doc)
+
+	copy(p, []byte(data))
+	return len(data), nil
 }
 
 func WebsiteReader(URL string) *WebsiteUrl {
 	return &WebsiteUrl{URL: URL}
 }
 
+func extractData(n *html.Node) string {
+	var data string
+
+	if n.Type == html.TextNode {
+		data += n.Data
+	}
+
+	for c := n.FirstChild; c != nil; c = c.NextSibling {
+		data += extractData(c)
+	}
+
+	return data
+}
+
 func main() {
-	reader := WebsiteReader("URL IN HERE!")
+	reader := WebsiteReader("URL IN HERE")
 
 	data := make([]byte, 1024)
+
+	if len(data) < 99999 {
+		data = append(data, make([]byte, 99999-len(data))...)
+	}
 
 	n, err := reader.Read(data)
 
@@ -32,5 +64,13 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Read %d bytes: %s\n", n, data[:n])
+	err = os.WriteFile("result.txt", data, 14754)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Result Created!")
+
+	fmt.Printf("Read %d\n", n)
 }
